f84b37ddc26d62c1e9b05db5375713dddaed57f3
Ildar <ildartimerbulatov@yandex.ru>
2025-01-07 14:59:12 +0300

    шестой коммит: md

--- Animals/import_sql.py
+++ Animals/import_sql.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from config import MYSQL_PASSWORD  # Импортируйте пароль из файла конфигурации
+from Animals.config import MYSQL_PASSWORD  # Импортируйте пароль из файла конфигурации
 
 # Подключение к базе данных
 conn = mysql.connector.connect(
@@ -14,11 +14,18 @@ cursor = conn.cursor()
 # Выполнение запроса
 cursor.execute("SELECT * FROM `Human Friends`")
 
+# Получение заголовков столбцов
+column_names = [i[0] for i in cursor.description]
+
 # Получение всех строк
 rows = cursor.fetchall()
 
 # Запись в файл
 with open(r'C:\Users\ildar\OneDrive\Desktop\pitomnik\Animals\Human Friends.txt', 'w', encoding='utf-8') as f:
+    # Запись заголовков столбцов
+    f.write(', '.join(column_names) + '\n')
+    
+    # Запись данных
     for row in rows:
         f.write(', '.join(map(str, row)) + '\n')
 
